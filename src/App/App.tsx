f133af21093024f6b86e4391620d6e04a9ca69bc
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-25 03:05:13 +0100

    Move app inside router

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,17 @@
 import { Header } from "src/components/PlayerHeaders";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Editor from "src/components/Editor";
 
 const documentId = "NEW_TEST";
 
 function App() {
   return (
-    <>
-      <Header documentId={documentId} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header documentId={documentId} />} />
+        <Route path="/edit" element={<Editor documentId={documentId} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
