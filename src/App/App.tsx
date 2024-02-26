0d5faf463b9b494426ab1e07524d73f5b6849916
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-26 22:16:37 +0100

    Change BrowserRouter to HashRouter

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,17 @@
 import { Header } from "src/components/PlayerHeaders";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Editor from "src/components/Editor";
 
 const documentId = "NEW_TEST";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Header documentId={documentId} />} />
         <Route path="/edit" element={<Editor documentId={documentId} />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
