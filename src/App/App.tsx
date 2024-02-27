04e063d4b541ddf3beb181ca3e8179305cd586ec
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-27 20:59:31 +0100

    Change routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "src/components/PlayerHeaders";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Editor from "src/components/Editor";
+import { Layout } from "src/components/Layout";
 
 const documentId = "NEW_TEST";
 
@@ -8,8 +9,10 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Header documentId={documentId} />} />
-        <Route path="/edit" element={<Editor documentId={documentId} />} />
+        <Route path="render" element={<Header documentId={documentId} />} />
+        <Route path="" element={<Layout />}>
+          <Route path="edit" element={<Editor documentId={documentId} />} />
+        </Route>
       </Routes>
     </HashRouter>
   );
