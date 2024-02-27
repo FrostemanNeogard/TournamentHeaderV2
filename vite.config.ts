a01cdd30b1c7556caa85c53129dfbef75c55d3a3
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-02-27 15:53:45 +0100

    Add "base" property

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/TournamentHeaderV2/",
   resolve: {
     alias: {
       src: "/src/",
