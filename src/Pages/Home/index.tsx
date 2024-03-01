57a793f516f2c604a74128a1c28998e90a7b6339
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 01:10:39 +0100

    Fix logic for header id input re-rendering

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import * as S from "./styled";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 type Verb = "edit" | "view" | "create";
 
@@ -12,72 +12,97 @@ export const HomePage = () => {
     setVerb(verb);
   };
 
-  const handleHeaderId = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setHeaderId(e.target.value);
+  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setHeaderId(value);
   };
 
-  const HeaderInteraction = (props: { verb: Verb }) => {
-    const FinalButton = () => {
-      switch (props.verb) {
-        case "create":
-          return (
-            <button>
-              <Link to={`edit/?id=${headerId}`}>Create</Link>
-            </button>
-          );
-        case "edit":
-          return (
-            <button>
-              <Link to={`edit/?id=${headerId}`}>Edit</Link>
-            </button>
-          );
-        case "view":
-          return (
-            <button>
-              <Link to={`render/?id=${headerId}`}>View</Link>
-            </button>
-          );
-      }
-    };
-
+  const memoizedHeaderInteraction = useMemo(() => {
     return (
-      <>
-        <label htmlFor="header-id">
-          Enter a name for the header you want to {props.verb}
-        </label>
-        <input
-          type="text"
-          name="header-id"
-          onChange={handleHeaderId}
-          value={headerId}
-        />
-        <button onClick={() => changeVerb(undefined)}>Back</button>
-        <FinalButton />
-      </>
+      <HeaderInteraction
+        headerId={headerId}
+        verb={verb}
+        onChangeHeaderId={handleIdChange}
+        onBack={() => changeVerb(undefined)}
+      />
     );
-  };
-
-  const HomeOptions = () => {
-    return (
-      <>
-        <p>What would you like to do?</p>
-        <button onClick={() => changeVerb("create")}>
-          Create a new header
-        </button>
-        <button onClick={() => changeVerb("edit")}>
-          Edit an existing header
-        </button>
-        <button onClick={() => changeVerb("view")}>
-          View an existing header
-        </button>
-      </>
-    );
-  };
+  }, [headerId, verb]);
 
   return (
     <S.Main>
       <h1>Tournament Header Homepage</h1>
-      {verb ? <HeaderInteraction verb={verb} /> : <HomeOptions />}
+      {verb ? (
+        memoizedHeaderInteraction
+      ) : (
+        <HomeOptions onChangeVerb={changeVerb} />
+      )}
     </S.Main>
+  );
+};
+
+const HeaderInteraction = (props: {
+  headerId: string | undefined;
+  verb: Verb | undefined;
+  onChangeHeaderId: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBack: () => void;
+}) => {
+  const FinalButton = () => {
+    switch (props.verb) {
+      case "create":
+        return (
+          <button>
+            <Link to={`edit/?id=${props.headerId}`}>Create</Link>
+          </button>
+        );
+      case "edit":
+        return (
+          <button>
+            <Link to={`edit/?id=${props.headerId}`}>Edit</Link>
+          </button>
+        );
+      case "view":
+        return (
+          <button>
+            <Link to={`render/?id=${props.headerId}`}>View</Link>
+          </button>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <>
+      <label htmlFor="header-id">
+        Enter an ID for the header you want to {props.verb}.
+      </label>
+      <input
+        type="text"
+        name="header-id"
+        onChange={props.onChangeHeaderId}
+        value={props.headerId}
+      />
+      <button onClick={props.onBack}>Back</button>
+      <FinalButton />
+    </>
+  );
+};
+
+const HomeOptions = (props: {
+  onChangeVerb: (verb: Verb | undefined) => void;
+}) => {
+  return (
+    <>
+      <p>What would you like to do?</p>
+      <button onClick={() => props.onChangeVerb("create")}>
+        Create a new header
+      </button>
+      <button onClick={() => props.onChangeVerb("edit")}>
+        Edit an existing header
+      </button>
+      <button onClick={() => props.onChangeVerb("view")}>
+        View an existing header
+      </button>
+    </>
   );
 };
