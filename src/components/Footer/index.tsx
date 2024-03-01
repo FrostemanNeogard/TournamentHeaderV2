1061086c3a0ac86a65d3555a529a4f4a56505bd0
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 05:18:18 +0100

    Change footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,13 @@ import * as S from "./styled";
 export const Footer = () => {
   return (
     <S.Footer>
-      <h1>App made by @funnyorangcat</h1>
+      <h1>
+        Tournament Header app created by{" "}
+        <a href="https://twitter.com/funnyorangcat" target="_blank">
+          @funnyorangcat
+        </a>
+        .
+      </h1>
     </S.Footer>
   );
 };
