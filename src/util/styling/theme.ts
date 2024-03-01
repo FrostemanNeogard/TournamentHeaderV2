3e16f68500da14628b5aa4fbc8e83d2ea7bf50c9
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 06:55:33 +0100

    Refactor text colors

--- src/util/styling/theme.ts
+++ src/util/styling/theme.ts
@@ -4,11 +4,16 @@ export const Theme = {
   },
 
   colors: {
+    text: {
+      primary: "#f2f2f2",
+      primaryDarkened: "#d2d2d2",
+
+      secondary: "#2d2d2d",
+      secondaryDarkened: "#1d1d1d",
+    },
+
     primary: "#555",
     primaryDarkened: "#444",
-
-    textPrimary: "#f2f2f2",
-    textSecondary: "#2d2d2d",
 
     secondary: "#1f1f1f",
     secondaryDarkened: "#1f1f1f",
