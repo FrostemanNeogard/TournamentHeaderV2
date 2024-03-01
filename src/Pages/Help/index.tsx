9dcdabc95980b1abb788278fc2c6d9278fc50543
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 06:50:19 +0100

    Update "Help" page

--- src/Pages/Help/index.tsx
+++ src/Pages/Help/index.tsx
@@ -4,11 +4,59 @@ export const Help = () => {
   return (
     <S.Main>
       <h1>How to use</h1>
-      <ol>
-        <li>yaahoooo</li>
-        <li>bweee</li>
-        <li>Step 3.</li>
-      </ol>
+
+      <div>
+        <h2>Creating a header</h2>
+        <ol>
+          <li>Go to the "Home" page and press "Create a new header".</li>
+          <li>
+            Enter an ID for the header you want to create. This will be the
+            "name" of your header. People can search for the ID to edit the
+            header.{" "}
+            <strong>
+              Only give out the ID to those you want to have editing access.
+            </strong>
+          </li>
+          <li>
+            Press "Proceed". This will create your header and redirect you to
+            the editor for your header.
+          </li>
+        </ol>
+      </div>
+
+      <div>
+        <h2>Editing an existing header:</h2>
+        <ol>
+          <li>Go to the "Home" page and press "Edit an existing header".</li>
+          <li>
+            Enter the ID for the header you want to edit, then press "Proceed".
+          </li>
+          <li>When in the editor, make any appropriate changes.</li>
+          <li>Press "Submit" to update your changes.</li>
+        </ol>
+      </div>
+
+      <div>
+        <h2>Inserting your header into streaming/recording software:</h2>
+        <ol>
+          <li>
+            Create a new header from the "Home" page (or edit an existing
+            header).
+          </li>
+          <li>While in the editor, press the "Copy render URL" button.</li>
+          <li>
+            Paste the copied URL into a browser source in your
+            streaming/recording software of choice.{" "}
+            <strong>
+              Make sure to set the width and height to 1920x1080 pixels.
+            </strong>
+          </li>
+          <li>
+            You're all set! You can now edit your header remotely from any
+            device via the editor.
+          </li>
+        </ol>
+      </div>
     </S.Main>
   );
 };
--- src/Pages/Help/styled.ts
+++ src/Pages/Help/styled.ts
@@ -1,8 +1,27 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
+  h1 {
+    font-weight: 700;
+    font-size: 2rem;
+    margin-bottom: 20px;
+  }
+
+  h2 {
+    font-weight: 700;
+    font-size: 1.5rem;
+  }
+
   ol {
     list-style: decimal;
     list-style-position: inside;
+
+    li {
+      margin: 10px 0;
+    }
+  }
+
+  strong {
+    font-weight: 700;
   }
 `;
