6849be8727a6291e74e10b65066e778b8038fd1f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 01:22:55 +0100

    Rename Header component and remove documentId props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,14 @@
-import { Header } from "src/Pages/Render";
+import { TournamentHeader } from "src/Pages/Render";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Editor from "src/Pages/Edit";
 import { Layout } from "src/components/Layout";
 import { HomePage } from "src/Pages/Home";
 
-const documentId = "NEW_TEST";
-
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="render" element={<Header documentId={documentId} />} />
+        <Route path="render" element={<TournamentHeader />} />
         <Route path="" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="edit" element={<Editor />} />
