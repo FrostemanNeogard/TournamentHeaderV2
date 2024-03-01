a71b6cde31d23142bc326745c2a12c504af8f9e2
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 06:32:06 +0100

    Create "Help" page

---
+++ src/Pages/Help/index.tsx
@@ -0,0 +1,14 @@
+import * as S from "./styled";
+
+export const Help = () => {
+  return (
+    <S.Main>
+      <h1>How to use</h1>
+      <ol>
+        <li>yaahoooo</li>
+        <li>bweee</li>
+        <li>Step 3.</li>
+      </ol>
+    </S.Main>
+  );
+};
---
+++ src/Pages/Help/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  ol {
+    list-style: decimal;
+    list-style-position: inside;
+  }
+`;
