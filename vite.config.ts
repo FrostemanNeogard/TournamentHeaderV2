1c812b78bd169c07a3b751efa8137f3ce4f7a085
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 07:43:06 +0100

    Change "Tournament Header V2" to "Tournament Header"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TournamentHeaderV2/",
+  base: "/TournamentHeader/",
   resolve: {
     alias: {
       src: "/src/",
