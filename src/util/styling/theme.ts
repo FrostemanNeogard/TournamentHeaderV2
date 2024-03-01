228bf52094ca1dce2342dde820458ecda8d4d75a
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 04:33:25 +0100

    Add colors to theme

--- src/util/styling/theme.ts
+++ src/util/styling/theme.ts
@@ -2,4 +2,24 @@ export const Theme = {
   breakpoints: {
     md: "768px",
   },
+
+  colors: {
+    primary: "#555",
+    primaryDarkened: "#444",
+
+    textPrimary: "#f2f2f2",
+    textSecondary: "#2d2d2d",
+
+    secondary: "#1f1f1f",
+    secondaryDarkened: "#1f1f1f",
+
+    tertiary: "#ddd",
+    tertiaryDarkened: "#ccc",
+
+    confirm: "#0f0",
+    confirmDarkened: "#0d0",
+
+    cancel: "#f00",
+    cancelDarkened: "#d00",
+  },
 };
