c41a2b97c47068ea0aae36998d33d7e525b8f297
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-03-01 04:41:41 +0100

    Create footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./styled";
+
+export const Footer = () => {
+  return (
+    <S.Footer>
+      <h1>App made by @funnyorangcat</h1>
+    </S.Footer>
+  );
+};
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  background-color: ${(props) => props.theme.colors.primary};
+  padding: 2rem;
+`;
